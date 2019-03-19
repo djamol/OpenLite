7ae922082db5178a498772511eee8ca406aefa42
DjAmolGroup <djamolpatil@gmail.com>
2019-03-19 22:33:03 +0530

    Update lsws.sh

--- lsws.sh
+++ lsws.sh
@@ -5,11 +5,11 @@ PWD_DIR=$SCRIPTPATH
 dbpass=admin
 cpu_num=`cat /proc/cpuinfo | grep processor | wc -l`
 
- sh $SCRIPTPATH/inc/openlite/jemalloc.sh 2>&1 | tee -a openl-jemalloc.txt
- sh $SCRIPTPATH/inc/openlite/openlite.sh 2>&1 | tee -a openl-openlite.txt
- sh $SCRIPTPATH/inc/openlite/nginx.sh 2>&1 | tee -a openl-jemalloc.txt
- sh $SCRIPTPATH/inc/openlite/php.sh 2>&1 | tee -a openl-php.txt
- sh $SCRIPTPATH/inc/openlite/ext-php-redis.sh 2>&1 | tee -a openl-redis.txt
- sh $SCRIPTPATH/inc/openlite/ext-php-mem.sh 2>&1 | tee -a openl-mem.txt
- sh $SCRIPTPATH/inc/openlite/ext-php-zendOPcache.sh 2>&1 | tee -a openl-zendOPcache.txt
-sh $SCRIPTPATH/inc/openlite/end.sh 2>&1 | tee -a openl-end.txt
+ sh $SCRIPTPATH/shell/jemalloc.sh 2>&1 | tee -a openl-jemalloc.txt
+ sh $SCRIPTPATH/shell/openlite.sh 2>&1 | tee -a openl-openlite.txt
+ sh $SCRIPTPATH/shell/nginx.sh 2>&1 | tee -a openl-jemalloc.txt
+ sh $SCRIPTPATH/shell/php.sh 2>&1 | tee -a openl-php.txt
+ sh $SCRIPTPATH/shell/ext-php-redis.sh 2>&1 | tee -a openl-redis.txt
+ sh $SCRIPTPATH/shell/ext-php-mem.sh 2>&1 | tee -a openl-mem.txt
+ sh $SCRIPTPATH/shell/ext-php-zendOPcache.sh 2>&1 | tee -a openl-zendOPcache.txt
+sh $SCRIPTPATH/shell/end.sh 2>&1 | tee -a openl-end.txt
