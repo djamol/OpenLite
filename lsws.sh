f560b2f54175e82f6c528a3c224d131c488b53d3
DjAmolGroup <djamolpatil@gmail.com>
2019-03-20 10:26:25 +0530

    Update lsws.sh

--- lsws.sh
+++ lsws.sh
@@ -8,6 +8,22 @@ mkdir $SCRIPTPATH/src
 dbpass=admin
 cpu_num=`cat /proc/cpuinfo | grep processor | wc -l`
 
+BUILD=log.txt
+
+if yum -y groupinstall "Development Tools"; then 
+echo -e "\033[33;32m yum -y groupinstall ----Development Tools Success ";date +"%r" >> $BUILD;echo "Success : Development Tools Install" >> $BUILD
+else
+echo -e "\033[33;31m yum -y groupinstall ----Development Tools Failed";date +"%r" >> $BUILD;echo "Failed : Development Tools Install" >> $BUILD
+fi
+if yum -y install sudo; then 
+echo -e "\033[33;32m Sudo Success";date +"%r" >> $BUILD;echo "Success : Sudo Install" >> $BUILD
+else
+echo -e "\033[33;31m Sudo Failed";date +"%r" >> $BUILD;echo "Failed : Sudo Install" >> $BUILD
+fi
+
+
+
+
  sh $SCRIPTPATH/shell/jemalloc.sh $SCRIPTPATH $cpu_num 2>&1 | tee -a openl-jemalloc.txt
  sh $SCRIPTPATH/shell/openlite.sh $SCRIPTPATH $cpu_num 2>&1 | tee -a openl-openlite.txt
  sh $SCRIPTPATH/shell/nginx.sh $SCRIPTPATH $cpu_num 2>&1 | tee -a openl-nginx.txt
