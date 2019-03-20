0da4acda79d514d5002f57c0e1d1cb8878f56fee
djamol <djamolpatil@gmail.com>
2019-03-20 09:34:07 +0530

    Update lsws.sh

--- lsws.sh
+++ lsws.sh
@@ -10,7 +10,7 @@ cpu_num=`cat /proc/cpuinfo | grep processor | wc -l`
 
  sh $SCRIPTPATH/inc/openlite/jemalloc.sh $SCRIPTPATH $cpu_num 2>&1 | tee -a openl-jemalloc.txt
  sh $SCRIPTPATH/inc/openlite/openlite.sh $SCRIPTPATH $cpu_num 2>&1 | tee -a openl-openlite.txt
- sh $SCRIPTPATH/inc/openlite/nginx.sh $SCRIPTPATH $cpu_num 2>&1 | tee -a openl-jemalloc.txt
+ sh $SCRIPTPATH/inc/openlite/nginx.sh $SCRIPTPATH $cpu_num 2>&1 | tee -a openl-nginx.txt
  sh $SCRIPTPATH/inc/openlite/php.sh $SCRIPTPATH $cpu_num 2>&1 | tee -a openl-php.txt
  sh $SCRIPTPATH/inc/openlite/ext-php-redis.sh $SCRIPTPATH $cpu_num 2>&1 | tee -a openl-redis.txt
  sh $SCRIPTPATH/inc/openlite/ext-php-mem.sh $SCRIPTPATH $cpu_num 2>&1 | tee -a openl-mem.txt
